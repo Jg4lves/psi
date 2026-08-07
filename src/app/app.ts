ef36c201014227c8a621f2d4509c4e0793e6aede
jgalves <hermesjgcode@gmail.com>
2026-08-07 11:12:54 -0300

    feat: add sobre section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { Hero } from './hero/hero';
 import { Background } from './shared/background/background';
 import { Identificacao } from './identificacao/identificacao';
+import { Sobre } from './sobre/sobre';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Hero, Background, Identificacao],
+  imports: [Hero, Background, Identificacao, Sobre],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/sobre/sobre.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sobre',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sobre.html',
+  styleUrl: './sobre.css',
+})
+export class Sobre {
+  openSection: string | null = null;
+
+  toggleSection(section: string): void {
+    this.openSection = this.openSection === section ? null : section;
+  }
+}
